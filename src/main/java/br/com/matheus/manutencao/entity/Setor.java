3b195659853fe7055994735ed69425c127062a42
MatheusBrayer <137443185+MatheusBrayer@users.noreply.github.com>
2026-08-12 11:10:16 -0300

    feat: criação de SetorController, metodos get e post

--- src/main/java/br/com/matheus/manutencao/entity/Setor.java
+++ src/main/java/br/com/matheus/manutencao/entity/Setor.java
@@ -18,6 +18,6 @@ public class Setor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome", nullable = false, length = 50)
+    @Column(name = "nome", nullable = false, length = 50, unique = true)
     private String nome;
 }
