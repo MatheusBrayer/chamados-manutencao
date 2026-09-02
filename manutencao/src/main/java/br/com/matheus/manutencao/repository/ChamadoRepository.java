85aab51fd97d9006487f8627504f5708a7916326
Matheus Brayer <Matheus.Brayer@vulcabras.com>
2026-09-02 09:31:38 -0300

    otimiza carregamento dos chamados

--- manutencao/src/main/java/br/com/matheus/manutencao/repository/ChamadoRepository.java
+++ manutencao/src/main/java/br/com/matheus/manutencao/repository/ChamadoRepository.java
@@ -1,8 +1,19 @@
 package br.com.matheus.manutencao.repository;
 
 import br.com.matheus.manutencao.entity.Chamado;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ChamadoRepository extends JpaRepository<Chamado, Long>, JpaSpecificationExecutor<Chamado> {
+public interface ChamadoRepository
+        extends JpaRepository<Chamado, Long>, JpaSpecificationExecutor<Chamado> {
+
+    @EntityGraph(attributePaths = {"maquina", "setor", "mecanico"})
+    Page<Chamado> findAll(
+            Specification<Chamado> spec,
+            Pageable pageable
+    );
 }
