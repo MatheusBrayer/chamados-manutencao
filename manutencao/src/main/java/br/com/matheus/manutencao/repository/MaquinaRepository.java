849ecd18f6a14e9c2c6096d9404f1bc0f8302e44
Matheus Brayer <Matheus.Brayer@vulcabras.com>
2026-09-02 10:06:57 -0300

    fix: corrige carregamento do setor nas maquinas

--- manutencao/src/main/java/br/com/matheus/manutencao/repository/MaquinaRepository.java
+++ manutencao/src/main/java/br/com/matheus/manutencao/repository/MaquinaRepository.java
@@ -1,12 +1,17 @@
 package br.com.matheus.manutencao.repository;
 
 import br.com.matheus.manutencao.entity.Maquina;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface MaquinaRepository extends JpaRepository <Maquina, Long> {
+public interface MaquinaRepository extends JpaRepository<Maquina, Long> {
 
+    @EntityGraph(attributePaths = {"setor"})
+    List<Maquina> findAll();
+
+    @EntityGraph(attributePaths = {"setor"})
     Optional<Maquina> findByNp(Long np);
-
 }
